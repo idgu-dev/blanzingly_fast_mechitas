ea400dd331ba9c4c8563192aa41263494b90f27f
idgu-dev <ivangusoria@gmail.com>
2022-05-14 12:38:24 +0200

    Map candles, log returns and variance for each file

--- src/main.rs
+++ src/main.rs
@@ -2,28 +2,29 @@ use std::error::Error;
 use std::process;
 use serde::Deserialize;
 extern crate statrs;
-use statrs::statistics;
-use statrs::distribution;
 use statrs::statistics::Statistics;
+use std::collections::HashMap;
+use std::time::{Duration, Instant};
 
 #[derive(Debug, Deserialize)]
+#[derive(Clone)]
 
 struct Candle {
-  date: String,
-  open: f64,
-  high: f64,
-  low: f64,
-  close: f64,
-  volume: f64,
+  // date: String,
+  Open: f64,
+  High: f64,
+  Low: f64,
+  Close: f64,
+  Volume: f64,
 }
 
-impl Candle {
-  fn print_candle(&self){
-    println!("Date:{}, Open:{}, High:{}, Low:{}, Close:{}, Volume:{}", 
-        self.date, self.open, self.high, self.low, self.close, self.volume
-    );
-  }
-}
+// impl Candle {
+//   fn print_candle(&self){
+//     println!("Date:{}, Open:{}, High:{}, Low:{}, Close:{}, Volume:{}", 
+//         self.date, self.open, self.high, self.low, self.close, self.volume
+//     );
+//   }
+// }
 
 fn read_file(file_path: &str) -> Result<Vec<Candle>, Box<dyn Error>> {
   let mut rdr = csv::Reader::from_path(file_path)?;
@@ -34,30 +35,39 @@ fn read_file(file_path: &str) -> Result<Vec<Candle>, Box<dyn Error>> {
 fn rolling_var(x: Vec<f64>, window: usize) -> Vec<f64>{
   let mut var_vec: Vec<f64> = Vec::<f64>::new();
   for i in 0..x.len()-window{
-      // let avg = x[i..i+window].iter().sum::<f64>() as f64 / window as f64;
-      // var_vec.push(f64::sqrt(((x[i+window-1] - avg).powf(2.0)) / window as f64));
-      var_vec.push((&x[i..i+window]).variance());
+    var_vec.push((&x[i..i+window]).variance());
   }
   return var_vec
 }
 
 fn main() {
-  let candles = match read_file("BTC_2015.csv") {
-    Ok(candles) => candles,
-    Err(err) => {
+  let now = Instant::now();
+  let dir_name = "Candles";
+  let mut candles_map: HashMap<String,Vec<Candle>> = HashMap::new();
+  let mut log_returns_map: HashMap<String, Vec<f64>> = HashMap::new();
+  let mut variance_map: HashMap<String, Vec<f64>> = HashMap::new();
+  let file_names = std::fs::read_dir(dir_name).unwrap()
+    .filter_map(|entry| {
+      entry.ok().and_then(|e| e.path().file_name()
+      .and_then(|n| n.to_str().map(|s| String::from(s)))
+      )
+    });
+  for file_name in file_names {
+    let path_to_read = dir_name.to_owned() + "/" + &file_name;
+    let candles = match read_file(&path_to_read) {
+      Ok(candles) => candles,
+      Err(err) => {
         println!("Error trying to read candles: {}", err);
         process::exit(1);
-    }
-  };
-
-  let mut log_returns = Vec::<f64>::new(); 
-  (1..candles.len()).for_each(|i| {
-    log_returns.push(candles[i].close.log(f64::exp(1.0)) - candles[i-1].close.log(f64::exp(1.0)));
-  });
-
-  let variance = rolling_var(log_returns, 30);
-  for v in variance.iter(){
-    println!("{}", v);
-  }
-
+      }
+    };
+    candles_map.insert(file_name.replace(".csv",""), candles.clone());
+    let mut log_returns = Vec::<f64>::new(); 
+    (1..candles.len()).for_each(|i| {
+      log_returns.push(candles[i].Close.log(f64::exp(1.0)) - candles[i-1].Close.log(f64::exp(1.0)));
+    });
+    log_returns_map.insert(file_name.replace(".csv", ""), log_returns.clone());
+    variance_map.insert(file_name.replace(".csv", ""), rolling_var(log_returns, 30));
+  } 
+  println!("{}", now.elapsed().as_millis());
 }
